bfda40183bc6087c39002e909fa996980e309cad
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-05-07 15:20:16 +0100

    Add parametric experiment for dropout

---
+++ src/utilities.py
@@ -0,0 +1,26 @@
+"""This module contains utility functions."""
+import numpy as np
+import seaborn as sns
+import torch
+
+
+def create_iris_data() -> tuple[np.ndarray, np.ndarray]:
+    """This returns the independent and the target features."""
+    # load data
+    iris_data = sns.load_dataset("iris")
+
+    # Preprocess the data
+    condlist = [
+        (iris_data["species"] == "setosa"),
+        (iris_data["species"] == "versicolor"),
+        iris_data["species"] == "virginica",
+    ]
+    choicelist = [0, 1, 2]
+    iris_data["target"] = np.select(condlist=condlist, choicelist=choicelist)
+
+    # Convert the data to Torch tensor
+    X = torch.Tensor(iris_data.loc[:, iris_data.columns[:4]].values)
+    y = torch.Tensor(iris_data["target"].values).long()
+
+    print(f"Shape of X: {X.shape}, Shape of X: {y.shape}")
+    return (X, y)
