6c585418c5bd4096a44b55eeabace3f2a37f5fa9
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-05-13 20:03:21 +0100

    chore: Add code for standardization

--- src/utilities.py
+++ src/utilities.py
@@ -1,6 +1,7 @@
 """This module contains utility functions."""
 import numpy as np
 import numpy.typing as npt
+import pandas as pd
 import seaborn as sns
 import torch
 
@@ -31,3 +32,25 @@ def create_iris_data() -> tuple[torch.Tensor, torch.Tensor]:
 def smooth(X: npt.NDArray[np.float64], k: int = 5) -> npt.NDArray[np.float64]:
     """This is used to smoothen the plot."""
     return np.convolve(X, np.ones(k) / k, mode="same")
+
+
+def load_data(*, filename: str, sep: str = ",") -> pd.DataFrame:
+    """This is used to load the data.
+
+    NB: Supported formats are 'csv' and 'parquet'.
+
+    Params;
+        filename (str): The filepath.\n
+        sep (str, default=","): The separator. e.g ',', '\t', etc \n
+
+    Returns:
+        data (pd.DataFrame): The loaded dataframe.
+    """
+    data = (
+        pd.read_csv(filename, sep=sep)
+        if filename.split(".")[-1] == "csv"
+        else pd.read_parquet(filename)
+    )
+    print(f"Shape of data: {data.shape}\n")
+
+    return data
