6bb9cfdd840ea78ff8399407e3f9a5a0f97686e3
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-05-15 08:04:47 +0100

    chore: Add data normalization notebook

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -53,3 +53,68 @@ class Standardizer:
         self.fit(X=X)
         X = self.transform(X=X)
         return X
+
+
+class Normalizer:
+    """This class is used to normalize the data.
+    i.e. the result has a min and max value of 0 and 1 by default."""
+
+    def __init__(self, min_value: float = 0, max_value: float = 1) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+        self._min = 0
+        self._max = 0
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(min_value={self.min_value}, " f"max_value={self.max_value})"
+        )
+
+    @staticmethod
+    def _normalize(
+        X: Union[pd.DataFrame, npt.NDArray[np.float_]],
+        min_: npt.NDArray[np.float_],
+        max_: npt.NDArray[np.float_],
+    ) -> float:
+        """This is used to normalize the data."""
+        return (X - min_) / (max_ - min_)
+
+    def _custom_normalize(
+        self,
+        X: Union[pd.DataFrame, npt.NDArray[np.float_]],
+        min_: npt.NDArray[np.float_],
+        max_: npt.NDArray[np.float_],
+    ) -> float:
+        """This is used to adjust the min and max values."""
+        x_scaled = self._normalize(X=X, min_=min_, max_=max_)
+        x_adjusted = self.min_value + x_scaled * (self.max_value - self.min_value)
+        return x_adjusted
+
+    def fit(
+        self, X: Union[pd.DataFrame, npt.NDArray[np.float_]], y=None
+    ) -> Union[pd.DataFrame, npt.NDArray[np.float_]]:
+        """This is used to learn the parameters,"""
+        self._min = np.zeros(shape=X.shape[1])
+        self._max = np.zeros(shape=X.shape[1])
+
+        for idx, var in enumerate(X.columns):
+            self._min[idx] = np.min(X[var])  # type: ignore
+            self._max[idx] = np.max(X[var])  # type: ignore
+
+        return self
+
+    def transform(
+        self, X: Union[pd.DataFrame, npt.NDArray[np.float_]], y=None
+    ) -> Union[pd.DataFrame, npt.NDArray[np.float_]]:
+        """This applies the transformation."""
+        # X = self._normalize(X=X, min_=self._min, max_=self._max)
+        X = self._custom_normalize(X=X, min_=self._min, max_=self._max)
+        return X
+
+    def fit_transform(
+        self, X: Union[pd.DataFrame, npt.NDArray[np.float_]], y=None
+    ) -> Union[pd.DataFrame, npt.NDArray[np.float_]]:
+        """This is used to learn the parameters and apply the transformation."""
+        self.fit(X=X)
+        X = self.transform(X=X)
+        return X
