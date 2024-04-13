22a3681fc87b6590d9288f4f0b882467e98f3b0c
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2024-04-13 17:12:19 +0100

    chore: Add logging using PyTorch Lighning

--- src/helper_plotting.py
+++ src/helper_plotting.py
@@ -1,6 +1,6 @@
 # Copied!
 import os
-from typing import Any, TypeAlias
+from typing import Any, Optional, TypeAlias
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -162,3 +162,73 @@ def show_examples(
             ax.axison = False
     plt.tight_layout()
     plt.show()
+
+
+def show_failures(
+    model: torch.nn.Module,
+    data_loader: DataLoader,
+    unnormalizer: Any | None = None,
+    class_dict: Optional[dict[int, str]] = None,
+    nrows: int = 3,
+    ncols: int = 5,
+    figsize: Optional[tuple[int, int]] = None,
+) -> tuple[plt.Figure, np.ndarray]:
+    failure_features: list[torch.Tensor] = []
+    failure_pred_labels: list[int] = []
+    failure_true_labels: list[int] = []
+
+    for batch_idx, (features, targets) in enumerate(data_loader):
+        with torch.inference_mode():
+            features = features
+            targets = targets
+            logits = model(features)
+            predictions = torch.argmax(logits, dim=1)
+
+        for i in range(features.shape[0]):
+            if targets[i] != predictions[i]:
+                failure_features.append(features[i])
+                failure_pred_labels.append(predictions[i])
+                failure_true_labels.append(targets[i])
+
+        if len(failure_true_labels) >= nrows * ncols:
+            break
+
+    features = torch.stack(failure_features, dim=0)
+    targets = torch.tensor(failure_true_labels)
+    predictions = torch.tensor(failure_pred_labels)
+
+    fig, axes = plt.subplots(
+        nrows=nrows, ncols=ncols, sharex=True, sharey=True, figsize=figsize
+    )
+
+    if unnormalizer is not None:
+        for idx in range(features.shape[0]):
+            features[idx] = unnormalizer(features[idx])
+    nhwc_img = np.transpose(features, axes=(0, 2, 3, 1))
+
+    if nhwc_img.shape[-1] == 1:
+        nhw_img = np.squeeze(nhwc_img.numpy(), axis=3)
+
+        for idx, ax in enumerate(axes.ravel()):
+            ax.imshow(nhw_img[idx], cmap="binary")
+            if class_dict is not None:
+                ax.title.set_text(
+                    f"P: {class_dict[predictions[idx].item()]}"
+                    f"\nT: {class_dict[targets[idx].item()]}"
+                )
+            else:
+                ax.title.set_text(f"P: {predictions[idx]} | T: {targets[idx]}")
+            ax.axison = False
+
+    else:
+        for idx, ax in enumerate(axes.ravel()):
+            ax.imshow(nhwc_img[idx])
+            if class_dict is not None:
+                ax.title.set_text(
+                    f"P: {class_dict[predictions[idx].item()]}"
+                    f"\nT: {class_dict[targets[idx].item()]}"
+                )
+            else:
+                ax.title.set_text(f"P: {predictions[idx]} | T: {targets[idx]}")
+            ax.axison = False
+    return fig, axes
